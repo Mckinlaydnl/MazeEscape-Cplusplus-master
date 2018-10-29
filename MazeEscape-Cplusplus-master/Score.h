ac8309742f3a10f6bbe4d35e847dc2e7fa14ffcc
Mckinlaydnl <43002081+Mckinlaydnl@users.noreply.github.com>
2018-10-29 16:34:03 +0000

    Added Score and Text

---
+++ MazeEscape-Cplusplus-master/Score.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// Library Includes
+#include "TextObject.h"
+
+class Score : public TextObject
+{
+public:
+	Score();	
+};
---
+++ MazeEscape-Cplusplus-master/TextObject.cpp
@@ -0,0 +1,15 @@
+// Project Includes
+#include "TextObject.h"
+
+TextObject::TextObject()
+	: GameObject() // Always initialise your parent class in your constructor
+	, m_text()
+{
+
+}
+
+
+void TextObject::Draw(sf::RenderTarget& _target)
+{
+	_target.draw(m_text);
+}
